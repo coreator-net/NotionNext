c6eae3eb239c6fe17c7161edb232f6de3232ac8d
epic.c <epic.cc@icloud.com>
2025-05-05 18:57:43 +0800

    update word

--- themes/nav/components/BlogPostItem.js
+++ themes/nav/components/BlogPostItem.js
@@ -24,7 +24,7 @@ const BlogPostItem = (props) => {
   } else {
     return <>
             <div id='uncategory' className='category text-lg pt-9 pb-4 first:pt-4 font-bold select-none flex justify-between  text-neutral-800 dark:text-neutral-400 p-2' key='uncategory'>
-                <span><i className={`text-base mr-2 ${group?.icon ? group?.icon : 'fas fa-hashtag'}`} />未分类</span>
+                <span><i className={`text-base mr-2 ${group?.icon ? group?.icon : 'fas fa-hashtag'}`} />未分類</span>
             </div>
             <div className='card-list grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'>
             {group?.items?.map(post => (
